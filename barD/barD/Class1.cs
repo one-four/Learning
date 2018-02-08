e6ac1b31f15df0ec864271d5cbe63f54eb5719e2
one-four <ananyev.do@gmail.com>
2018-02-08 22:23:37 +0200

    3-d commit

---
+++ barD/barD/Class1.cs
@@ -0,0 +1,21 @@
+﻿//using RGiesecke.DllExport;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace barD
+{ 
+    class Test
+    {
+        [DllExport]
+//        [DllExport("add", CallingConvention = CallingConvention.Cdecl)]
+        public static int Add(int left, int right)
+        {
+            return left + right;
+        }
+    }
+}
