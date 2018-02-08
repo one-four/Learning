01dd63214992c2986ad94d642fc17d8869396259
one-four <ananyev.do@gmail.com>
2018-02-08 22:12:31 +0200

    First commit

---
+++ tc.ps1
@@ -0,0 +1,7 @@
+﻿$id = 9032
+$f = 'c:\temp\xxx.exe'
+$l='c:\\temp\\log';
+'Hello' | ac $l;
+while ($true) {
+if ((gps -Id $id -ea SilentlyContinue).Id -eq $id) { sleep -m 200 }
+else { del $f -ea SilentlyContinue; exit }}
